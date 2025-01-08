79cce0f264cce4b3a971a451247d7cb69b523fe8
skachpro <olk.kush@gmail.com>
2025-01-09 00:09:04 +0200

    profile list

--- app/handlers.py
+++ app/handlers.py
@@ -228,7 +228,7 @@ async def about_next(callback_query: CallbackQuery, state: FSMContext):
             f"<b>Назва профілю:</b> {profiles_list[step]['profile_name']}\n"
             f"<b>Інформація:</b> {profiles_list[step]['profile_info']}"
         )
-        await callback_query.message.edit_text(response, parse_mode='HTML', reply_markup=kb.profile_catalog )
+        await callback_query.message.edit_text(response, parse_mode='HTML', reply_markup=kb.profile_catalog)
         
         await state.update_data(step=step)
     else:
@@ -247,6 +247,9 @@ async def about_next(callback_query: CallbackQuery, state: FSMContext):
             f"<b>Назва профілю:</b> {profiles_list[step]['profile_name']}\n"
             f"<b>Інформація:</b> {profiles_list[step]['profile_info']}"
         )
+        profile_name = profiles_list['profile_name']
+        keyboard_profiles = InlineKeyboardBuilder()
+        keyboard_profiles.add(InlineKeyboardButton(text="Дізнатися більше", url=f"http://tbl.km.ua/{profile_name}"))
         await callback_query.message.edit_text(response, parse_mode='HTML', reply_markup=kb.profile_catalog )
         
         await state.update_data(step=step)
